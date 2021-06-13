9f19b10fccf60c23953b84a4b562d4d0c3c97a23
cmalsenido <cmalsenido@gmail.com>
2021-06-13 05:09:50 -0700

    First commit

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ resource "aws_instance" "web" {
   ami = "ami-0ac80df6eff0e70b5"
   instance_type = "t2.micro"
   
-  // tags = {
-  //   Name = "devops-one"
-  // }
+   tags = {
+   Name = "devops-one"
+  }
 }
 
 output "public_ip" {
